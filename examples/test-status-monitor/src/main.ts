83d0a0c05727cfc12f104e32718c666980b3fe2a
robince <robince885@gmail.com>
2019-04-26 23:43:39 +0200

    Fix example to work with Nestjs v6

--- examples/test-status-monitor/src/main.ts
+++ examples/test-status-monitor/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(3001);
 }
 bootstrap();
